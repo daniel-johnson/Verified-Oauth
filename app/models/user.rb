6665c01a00049662308a8ea7d7b86242cd823f56
Daniel Johnson <janiel.dohnson@gmail.com>
2017-03-26 12:39:45 -0700

    Removed that verification logic. It's premature.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,2 @@
 class User < ApplicationRecord
-	before_save :check_if_verified unless self.verified
-
-	private
-
-	def check_if_verfied
-		if self.address_verified && self.phone_verified && self.email_verified
-			self.verified = true
-		end
-	end
 end
